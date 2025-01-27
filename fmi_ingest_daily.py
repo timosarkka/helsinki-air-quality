cebc7b753c36141220b20aec95250ce48cdea593
timosarkka <t@timosarkka.com>
2025-01-27 21:52:11 +0200

    Added API call to fetch Helsinki's air quality data

--- fmi_ingest_daily.py
+++ fmi_ingest_daily.py
@@ -1,1 +1,9 @@
 import requests
+
+def fetch_air_quality_data():
+    url = 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::airquality::hourly::simple&region=helsinki'
+    response = requests.get(url)
+    airq_output = response.text
+    print(airq_output)
+
+fetch_air_quality_data()
