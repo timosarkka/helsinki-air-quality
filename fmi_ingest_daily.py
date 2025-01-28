6cd6c34af778602248476a923b1263841296fb8d
timosarkka <t@timosarkka.com>
2025-01-28 19:19:34 +0200

    Added function to parse air quality XML data

--- fmi_ingest_daily.py
+++ fmi_ingest_daily.py
@@ -1,9 +1,17 @@
 import requests
+import pandas
+import xml.etree.ElementTree as ET
 
 def fetch_air_quality_data():
     url = 'https://opendata.fmi.fi/wfs?service=WFS&version=2.0.0&request=getFeature&storedquery_id=fmi::observations::airquality::hourly::simple&region=helsinki'
     response = requests.get(url)
     airq_output = response.text
-    print(airq_output)
+    return airq_output
 
-fetch_air_quality_data()
+def parse_air_quality_data():
+    root = ET.fromstring(xml_data)
+    namespace = {'gml': 'http://www.opengis.net/gml/3.2'}
+    records = []
+
+xml_data = fetch_air_quality_data()
+parsed_data = parse_air_quality_data(xml_data)
