4628e72217d380653a426e7f8791559cd07070be
Timo Särkkä <t@timosarkka.com>
2025-02-20 19:24:05 +0200

    Changed scheduling cadence to 12 hours

--- dags/run_extract_fmi_aq.py
+++ dags/run_extract_fmi_aq.py
@@ -17,7 +17,7 @@ with DAG(
     dag_id='run_extract_fmi_aq',
     default_args=default_args,
     description='Run a Python script to extract FMI air quality data and save to Snowflake raw layer.',
-    schedule_interval='0 * * * *', 
+    schedule_interval='0 */12 * * *', 
     start_date=datetime(2025, 1, 1),
     catchup=False
 ) as dag:
@@ -28,3 +28,14 @@ with DAG(
         bash_command="python /Users/timosarkka/Projects/finnish-air-quality/extract/fmi_aq_ingest_daily.py",  
         cwd="/Users/timosarkka/Projects/finnish-air-quality/extract"  
     )
+
+    # Task 2: Run a dbt job
+    run_dbt_job = DbtCloudRunJobOperator(
+        task_id='run_dbt_job',
+        job_id=12345,
+        account_id=67890,
+        wait_for_completion=True
+    )
+    
+    # Set task dependencies
+    run_extract_python >> run_dbt_job
